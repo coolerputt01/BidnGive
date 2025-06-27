c1469599a15f847e144ebf4ef751e8115339a008
coolerputt01 <olumideadekolu9@gmail>
2025-06-27 10:33:58 -0700

    next commit

--- user/backenduser/backend/bids/urls.py
+++ user/backenduser/backend/bids/urls.py
@@ -1,7 +1,7 @@
 from rest_framework.routers import DefaultRouter
 from .views import BidViewSet 
 from .views import UploadProofView, ConfirmReceiverView
-from django.urls import path
+from django.urls import path , run_auto_merge, auto_block
 
 router = DefaultRouter()
 router.register(r'', BidViewSet, basename='bid')  # register at root
@@ -9,6 +9,8 @@ router.register(r'', BidViewSet, basename='bid')  # register at root
 urlpatterns = [
     path('upload-proof/<int:pk>/', UploadProofView.as_view(), name='upload-proof'),
     path('confirm-receive/<int:bid_id>/', ConfirmReceiverView.as_view(), name='confirm-receive'),
+    path('api/cron/merge/', run_auto_merge),
+    path('api/cron/bonus/', auto_block),
 ]
 
 urlpatterns += router.urls
--- user/backenduser/backend/bids/views.py
+++ user/backenduser/backend/bids/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets, permissions, status , generics
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -8,6 +8,25 @@ from rest_framework.parsers import MultiPartParser
 from .models import Bid
 from .serializers import BidSerializer , BidPaymentConfirmSerializer
 from django.utils import timezone
+from django.core.management import call_command
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def run_auto_merge(request):
+    if request.GET.get('key') != 'biddedkey':
+        return Response({'error': 'Unauthorized'}, status=403)
+    
+    call_command('auto_merge')
+    return Response({'status': 'Auto merge ran successfully'})
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def auto_block(request):
+    if request.GET.get('key') != 'expiredkey':
+        return Response({'error': 'Unauthorized'}, status=403)
+
+    call_command('auto_block_expired')
+    return Response({'status': 'Auto Block sent'})
 
 class BidViewSet(viewsets.ModelViewSet):
     queryset = Bid.objects.all()
