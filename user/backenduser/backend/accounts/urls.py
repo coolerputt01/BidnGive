6d72843e54cf18e917757c6a665eb787cb6a6440
coolerputt01 <olumideadekolu9@gmail>
2025-07-03 09:57:27 -0700

    next commit

--- user/backenduser/backend/accounts/urls.py
+++ user/backenduser/backend/accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
-from .views import RegisterView, CustomTokenObtainPairView, VerifyEmailOTPView,verify_whatsapp_otp,UserProfileView,enter_auction_room,ResendEmailOTP
+from .views import RegisterView, CustomTokenObtainPairView, VerifyEmailOTPView,UserProfileView,enter_auction_room,ResendEmailOTP
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', CustomTokenObtainPairView.as_view(), name='login'),
